e21816d22575cbdccca4df13a4dcb9f7b326d96f
raptor-minds <846192235@qq.com>
2020-02-26 00:07:39 +0800

    test LGraph

--- graph/LGraph_test.go
+++ graph/LGraph_test.go
@@ -51,3 +51,34 @@ func TestCreateLinkedGraph(t *testing.T) {
 
 	DFS(LGraph, 2)
 }
+
+func TestDFS(t *testing.T) {
+	LGraph := CreateLinkedGraph(10)
+
+	InsertLinkedEdge(LGraph, &ENode{
+		Weight: 1,
+		V1:     2,
+		V2:     5,
+	})
+	InsertLinkedEdge(LGraph, &ENode{
+		Weight: 1,
+		V1:     2,
+		V2:     7,
+	})
+
+	InsertLinkedEdge(LGraph, &ENode{
+		Weight: 1,
+		V1:     8,
+		V2:     9,
+	})
+
+	InsertLinkedEdge(LGraph, &ENode{
+		Weight: 1,
+		V1:     6,
+		V2:     9,
+	})
+
+	PrintLGraph(LGraph)
+
+	DFS(LGraph, 2)
+}
